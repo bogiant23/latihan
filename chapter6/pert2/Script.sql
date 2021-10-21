99b5e745fc033893822105ea4ac2be606e390f1e
bogiant23 <raskarowanabogiant@gmail.com>
2021-10-21 23:01:46 +0700

    add query SIDU

--- chapter6/pert2/Script.sql
+++ chapter6/pert2/Script.sql
@@ -4,3 +4,27 @@ create TABLE users (
  password VARCHAR not null,
  name VARCHAR null
 );
+
+create TABLE roles (
+ id SERIAL primary key,
+ name VARCHAR unique not null,
+ created_at TIMESTAMP default now() not NULL
+);
+
+DROP TABLE roles;
+
+insert into users (email,password) 
+values ('smaslul@binar.co.id', '$2a$12$qlMz6vHGc0Paw.91zzOdr./RkCHxO.Q712MN1cuBsezohtOWHMn8O');
+
+select id, email, fullname from users
+where id in (1,4,10);
+
+update users 
+set
+ email = 'iniakanterganti@binar.co.id',
+ fullname = 'Siapa Saya',
+ full_address = 'Jalan Jalan ko sendirian'
+where id = 1;
+
+delete from users where email = 'ani@binar.co.id';
+
