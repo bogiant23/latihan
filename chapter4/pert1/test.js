fea64dcad377734244ddb011b2e0979036bc844b
Bogiant Raskarowana <bogiant.raskarowana@bintang.com>
2021-11-29 16:18:08 +0700

    scope

--- chapter4/pert1/test.js
+++ chapter4/pert1/test.js
@@ -6,4 +6,26 @@ console.log(_);
 
 var namaPengguna= 'bogiant raskarowana';
 let topic1 = 'JavaScript Fundamental'
+console.log(namaPengguna)
+console.log(topic1)
 
+//scope
+var diskon = 500
+if(true){
+    var diskon = 300
+}
+console.log(diskon)
+
+var diskon2 = 500
+function diskonScope(){
+    var diskon2 = 300
+    console.log(diskon)
+}
+diskonScope()
+console.log(diskon2)
+
+let diskon3 = 500
+if(true){
+    let diskon = 300
+}
+console.log(diskon3)
